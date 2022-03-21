9917921f038c2876a8f71021e88c8d4745cd7679
Алексей Малков <kolectoran2@gmail.com>
2022-03-21 10:54:17 +0500

    Реализован файловый браузер

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,59 +32,118 @@ namespace DocxWordSearcher
             this.DocxFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
             this.buttonDirectoryPicker = new System.Windows.Forms.Button();
             this.textBox_searchCriteria = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelTextSearch = new System.Windows.Forms.Label();
             this.labelOutput = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.listBoxFolder = new System.Windows.Forms.ListBox();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonDirectoryPicker
             // 
             this.buttonDirectoryPicker.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonDirectoryPicker.Location = new System.Drawing.Point(4, 334);
+            this.buttonDirectoryPicker.Location = new System.Drawing.Point(254, 58);
             this.buttonDirectoryPicker.Name = "buttonDirectoryPicker";
-            this.buttonDirectoryPicker.Size = new System.Drawing.Size(329, 31);
+            this.buttonDirectoryPicker.Size = new System.Drawing.Size(131, 28);
             this.buttonDirectoryPicker.TabIndex = 0;
-            this.buttonDirectoryPicker.Text = "Выбрать папку с файлами для поиска";
+            this.buttonDirectoryPicker.Text = "Выбрать папку";
             this.buttonDirectoryPicker.UseVisualStyleBackColor = true;
             this.buttonDirectoryPicker.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox_searchCriteria
             // 
             this.textBox_searchCriteria.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_searchCriteria.Location = new System.Drawing.Point(4, 301);
+            this.textBox_searchCriteria.Location = new System.Drawing.Point(4, 414);
             this.textBox_searchCriteria.Name = "textBox_searchCriteria";
-            this.textBox_searchCriteria.Size = new System.Drawing.Size(329, 27);
+            this.textBox_searchCriteria.Size = new System.Drawing.Size(381, 27);
             this.textBox_searchCriteria.TabIndex = 1;
             // 
-            // label1
+            // labelTextSearch
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(0, 279);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 19);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Текст для поиска";
+            this.labelTextSearch.AutoSize = true;
+            this.labelTextSearch.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTextSearch.Location = new System.Drawing.Point(0, 392);
+            this.labelTextSearch.Name = "labelTextSearch";
+            this.labelTextSearch.Size = new System.Drawing.Size(136, 19);
+            this.labelTextSearch.TabIndex = 2;
+            this.labelTextSearch.Text = "Текст для поиска";
             // 
             // labelOutput
             // 
             this.labelOutput.AutoSize = true;
             this.labelOutput.BackColor = System.Drawing.SystemColors.Control;
             this.labelOutput.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelOutput.Location = new System.Drawing.Point(42, 23);
+            this.labelOutput.Location = new System.Drawing.Point(68, 22);
             this.labelOutput.Name = "labelOutput";
             this.labelOutput.Size = new System.Drawing.Size(247, 18);
             this.labelOutput.TabIndex = 3;
             this.labelOutput.Text = "Выберите папку с файлами Docx";
             this.labelOutput.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSearch.Location = new System.Drawing.Point(4, 447);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(381, 52);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPath.Location = new System.Drawing.Point(4, 59);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(244, 27);
+            this.textBoxPath.TabIndex = 6;
+            // 
+            // listBoxFolder
+            // 
+            this.listBoxFolder.FormattingEnabled = true;
+            this.listBoxFolder.Location = new System.Drawing.Point(4, 125);
+            this.listBoxFolder.Name = "listBoxFolder";
+            this.listBoxFolder.Size = new System.Drawing.Size(192, 264);
+            this.listBoxFolder.TabIndex = 7;
+            this.listBoxFolder.DoubleClick += new System.EventHandler(this.listBoxFolder_DoubleClick);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(202, 125);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(183, 264);
+            this.listBoxFiles.TabIndex = 8;
+            this.listBoxFiles.DoubleClick += new System.EventHandler(this.listBoxFiles_DoubleClick);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBack.Location = new System.Drawing.Point(4, 92);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(89, 27);
+            this.buttonBack.TabIndex = 9;
+            this.buttonBack.Text = "<--";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(338, 377);
+            this.ClientSize = new System.Drawing.Size(388, 503);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.listBoxFolder);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.labelOutput);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTextSearch);
             this.Controls.Add(this.textBox_searchCriteria);
             this.Controls.Add(this.buttonDirectoryPicker);
             this.Name = "FormMain";
@@ -99,8 +158,13 @@ namespace DocxWordSearcher
         private System.Windows.Forms.FolderBrowserDialog DocxFolderBrowser;
         private System.Windows.Forms.Button buttonDirectoryPicker;
         private System.Windows.Forms.TextBox textBox_searchCriteria;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTextSearch;
         private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ListBox listBoxFolder;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonBack;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 using DocxSearcher;
+using DocumentFormat.OpenXml.Packaging;
 
 namespace DocxWordSearcher
 {
@@ -19,39 +20,111 @@ namespace DocxWordSearcher
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void FindWordInFile()
         {
 
+        }
+
+        private void FindWords()
+        {
             if (textBox_searchCriteria.Text != "")
             {
-                if (DocxFolderBrowser.ShowDialog() == DialogResult.OK)
-                {
-                    string Path = DocxFolderBrowser.SelectedPath;
+                
+                string Path = textBoxPath.Text;
 
-                    File.SetAttributes(Path, FileAttributes.Normal);
-                    //TODO: разобраться как List записывать в файлы txt
+                File.SetAttributes(Path, FileAttributes.Normal);
+                //TODO: разобраться как List записывать в файлы txt
 
-                    string WholeThing = "";
+                string WholeThing = "";
 
-                    Console.WriteLine("_______________________________________");
-                    List<string> FoundInFiles = DocxSearch.FindKeywordInDir(textBox_searchCriteria.Text, Path, true);
-                    FoundInFiles.ForEach(p => Console.WriteLine(p + " " + "!!!"));
-                    Console.WriteLine("_______________________________________");
+                Console.WriteLine("_______________________________________");
+                List<string> FoundInFiles = DocxSearch.FindKeywordInDir(textBox_searchCriteria.Text, Path, true);
+                FoundInFiles.ForEach(p => Console.WriteLine(p + " " + "!!!"));
+                Console.WriteLine("_______________________________________");
 
-
-                    foreach (string matchingFiles in DocxSearch.FindKeywordInDir(textBox_searchCriteria.Text, Path, true))
+                foreach (string matchingFiles in DocxSearch.FindKeywordInDir(textBox_searchCriteria.Text, Path, true))
                     {
-                        WholeThing += matchingFiles;
-                        WholeThing += "\n";
+                    WholeThing += matchingFiles;
+                    WholeThing += "\n";
                     }
 
-                    string ResultInTxtPath = Path + "//Результат поиска.txt";
+                string ResultInTxtPath = Path + "\\Результат поиска.txt";
 
-                    File.WriteAllText(ResultInTxtPath, WholeThing);
-                }
+                File.WriteAllText(ResultInTxtPath, WholeThing);
+
             }
             else
                 MessageBox.Show("Введите текст для поиска.");
+        }
+
+        private void FolderBrowser(string path)
+        {
+            DirectoryInfo ThisDirectory = new DirectoryInfo(path);
+
+            listBoxFolder.Items.Clear();
+            listBoxFiles.Items.Clear();
+
+            //string[] AllDirectories = Directory.GetDirectories(path);
+            // int AmmountOfDirectories = 0; //AllDirectories.Length;
+
+            foreach(DirectoryInfo Directory in ThisDirectory.GetDirectories())
+            {
+                listBoxFolder.Items.Add(Directory.Name);
+            }
+            foreach(FileInfo File in ThisDirectory.GetFiles())
+            {
+                listBoxFiles.Items.Add(File.Name);
+            }
+
+            textBoxPath.Text = ThisDirectory.FullName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DocxFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string Path = DocxFolderBrowser.SelectedPath;
+
+                textBoxPath.Text = Path;
+
+                FolderBrowser(Path);
+            }
+        }
+        private void listBoxFolder_DoubleClick(object sender, EventArgs e)
+        {
+            DirectoryInfo DirectoryChosen = new DirectoryInfo(textBoxPath.Text + "\\" + listBoxFolder.SelectedItem.ToString());
+            FolderBrowser(DirectoryChosen.FullName);
+        }
+
+        private void buttonSearch_click(object sender, EventArgs e)
+        {
+            FindWords();
+        }
+
+        private void listBoxFiles_DoubleClick(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(textBoxPath.Text + "\\" + listBoxFiles.SelectedItem.ToString());
+
+            if (file.Exists)
+                if (DocxSearch.FindKeyword(textBox_searchCriteria.Text, file.FullName))
+                    if (MessageBox.Show("Искомое слово найдено. Открыть файл?", "Результат поиска", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                    {
+                        //file.Open(FileMode.Open);
+                        WordprocessingDocument.Open(file.FullName, true);
+                        string path = file.FullName.ToString();
+                    }
+                        
+                    else
+                        MessageBox.Show("Слово не найдено.", "Результат поиска", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                else
+                    MessageBox.Show("Файл несуществует, зафиксируйте ошибку в каком-либо документе для разработчика.", "Ошибка обращения к файлу", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
